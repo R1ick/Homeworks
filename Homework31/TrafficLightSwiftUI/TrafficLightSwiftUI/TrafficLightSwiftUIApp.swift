cc068cd4533acfbe67cf1d1081122a2435663bbc
Yaroslav Antonovich <yantonovich@apro.ai>
2022-03-30 08:28:04 +0300

    SwiftUI homework created

---
+++ Homework31/TrafficLightSwiftUI/TrafficLightSwiftUI/TrafficLightSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrafficLightSwiftUIApp.swift
+//  TrafficLightSwiftUI
+//
+//  Created by Ярослав Антонович on 30.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct TrafficLightSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

f3780ef56a8eeb49a699c14eeeddec58fb32a100
Yaroslav Antonovich <yantonovich@apro.ai>
2022-04-04 08:21:12 +0300

    Add initial for homework 32

---
+++ Homework32/ViewColorsSwiftUI/ViewColorsSwiftUI/ViewColorsSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewColorsSwiftUIApp.swift
+//  ViewColorsSwiftUI
+//
+//  Created by Ярослав Антонович on 04.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct ViewColorsSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
